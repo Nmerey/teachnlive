928a0cda2fba1d99c455258e37215a055467ab37
Nmerey <merey.nurlan@gmail.com>
2018-05-15 15:51:47 +0800

    Google Auth Add

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'home#index'
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 
 end
