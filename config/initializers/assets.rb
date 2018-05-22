7e16ee83e6499c63df3731cb007dc63787d58f81
Nmerey <merey.nurlan@gmail.com>
2018-05-22 15:30:46 +0800

    Zephs code is so awesome

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( absentnot.css )
 Rails.application.config.assets.precompile += %w( entry_page.css )
 Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( hover.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
