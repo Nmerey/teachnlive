585794d9f5803d01eaf7e3dca58a016931e95512
syami-ifcon <syamim@ifcon.tech>
2018-05-22 23:41:30 +0800

    merging all data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < Clearance::SessionsController
   else
     user = User.create_with_auth_and_hash(authentication, auth_hash)
     # you are expected to have a path that leads to a page for editing user details
-    @next = root_path
+    @next = lectures_path
     @notice = "User created. Please confirm or edit details"
   end
 
